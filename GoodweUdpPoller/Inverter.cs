6f94f364be90e6377aae2694cb40648692b9d0b9
Koen van Leeuwen Logiqs B.V <koen@logiqs.nl>
2021-05-19 16:33:38 +0200

    Add  pvoutput code

--- GoodweUdpPoller/Inverter.cs
+++ GoodweUdpPoller/Inverter.cs
@@ -1,8 +1,43 @@
-﻿namespace GoodweUdpPoller
+﻿using System;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace GoodweUdpPoller
 {
     public class Inverter
     {
-        public string Ip { get; set; }
-        public byte[] DiscoverData { get; set; }
+        private byte[] _discoverData;
+        public string ResponseIp { get; set; }
+
+        [JsonIgnore]
+        public byte[] DiscoverData
+        {
+            get => _discoverData;
+            set
+            {
+                _discoverData = value;
+                SetProperties(value);
+            }
+        }
+
+        private void SetProperties(byte[] value)
+        {
+            try
+            {
+                var stringData = Encoding.UTF8.GetString(value);
+                var segments = stringData.Split(",");
+                if (segments.Length < 3) return;
+                Host = segments[0];
+                Mac = segments[1];
+                Ssid = segments[2];
+            }
+            catch (Exception) { /*we have no control over what devices reply, so if the properties are unset that's a sign it is not a Goodwe inverter.*/ }
+        }
+
+        public string Ssid { get; set; }
+
+        public string Mac { get; set; }
+
+        public string Host { get; set; }
     }
 }
--- GoodweUdpPoller/Program.cs
+++ GoodweUdpPoller/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -6,32 +9,80 @@ namespace GoodweUdpPoller
 {
     class Program
     {
+        private static readonly HttpClient _client = new HttpClient();
 
         /// <summary>
-        /// 
+        /// Tool for querying Goodwe inverters over the local network.
+        /// Without options set, will try to discover any responding inverters on the network and display current telemetry for the last one.
+        /// Intended use is to invoke in a cronjob every 5 minutes to update PVOutput.org, but can be adapted to other uses as it outputs json by default.
         /// </summary>
-        /// <param name="host">IP address or hostname of the inverter.
+        /// <param name="host">IP address, hostname or subnet broadcast address of the inverter.
         /// If unset, will broadcast a discovery packet to find any compatible inverter.</param>
         /// <param name="timeout">Listen timeout for replies</param>
-        public static async Task Main(string host = null, int timeout = 1000)
+        /// <param name="pvoutputSystemId">System Id for API access on pvoutput.org, see https://pvoutput.org/help/api_specification.html </param>
+        /// <param name="pvoutputApikey">API key for API access on pvoutput.org, see https://pvoutput.org/help/api_specification.html </param>
+        /// <param name="pvoutputRequestUrl">optional url to post to</param>
+        public static async Task Main(
+            string host = null,
+            int timeout = 1000,
+            int pvoutputSystemId = 0,
+            string pvoutputApikey = "",
+            string pvoutputRequestUrl = "https://pvoutput.org/service/r2/addstatus.jsp")
         {
             var listenTimeout = TimeSpan.FromMilliseconds(timeout);
             var poller = new GoodwePoller(listenTimeout);
             if (host == null)
             {
-                var foundInverters = poller.DiscoverInvertersAsync();
-                await foreach (var foundInverter in foundInverters)
+                await foreach (var foundInverter in poller.DiscoverInvertersAsync())
                 {
-                    Console.WriteLine($"Found: {foundInverter.Ip}");
+                    if (foundInverter.Ssid == null /*== not a Goodwe inverter*/)
+                        continue;
+
+                    WriteObject(foundInverter);
+                    host = foundInverter.ResponseIp;
                 }
             }
-            else
+
+            if (host == null)
+                throw new ArgumentException("No host given on command line and none discovered, nothing to do. Either make sure your router doesn't block broadcasts or discover the IP with, for example, the Goodwe app");
+            if (pvoutputSystemId <= 0 ^ string.IsNullOrEmpty(pvoutputApikey))
+                throw new ArgumentException("Both systemid and apikey need to be set to upload to pvoutput");
+
+            var response = await poller.QueryInverter(host);
+            WriteObject(response);
+
+            if (pvoutputSystemId > 0)
+                await PostToPvOutput(response, pvoutputSystemId, pvoutputApikey, pvoutputRequestUrl);
+        }
+
+        private static async Task PostToPvOutput(InverterTelemetry inverterStatus, int pvOutputSystemId,
+            string pvOutputApikey, string pvOutputRequestUrl)
+        {
+            var values = new Dictionary<string, string>
             {
-                Console.WriteLine($"Querying {host}");
-                var response = await poller.QueryInverter(host);
-                var serialized = JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true });
-                Console.WriteLine(serialized);
-            }
+                { "d", inverterStatus.Timestamp.ToString("yyyyMMdd") },
+                { "t", inverterStatus.Timestamp.ToString("HH:mm") },
+                { "v1", (inverterStatus.EnergyLifetime*1000).ToString(CultureInfo.InvariantCulture) },
+                { "c1", "1" /*Lifetime energy is cumulative*/},
+                { "v2", inverterStatus.Power.ToString(CultureInfo.InvariantCulture) },
+                { "v5", inverterStatus.Temperature.ToString(CultureInfo.InvariantCulture) },
+                { "v6", inverterStatus.Vac.ToString(CultureInfo.InvariantCulture) },
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            _client.DefaultRequestHeaders.Add("X-Pvoutput-Apikey", pvOutputApikey);
+            _client.DefaultRequestHeaders.Add("X-Pvoutput-SystemId", pvOutputSystemId.ToString(CultureInfo.InvariantCulture));
+            var response = await _client.PostAsync(pvOutputRequestUrl, content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"<={responseString}");
+            response.EnsureSuccessStatusCode();
+        }
+
+        private static void WriteObject(object toWrite)
+        {
+            var serialized = JsonSerializer.Serialize(toWrite, new JsonSerializerOptions { WriteIndented = true });
+            Console.WriteLine(serialized);
         }
     }
 }
